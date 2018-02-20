b3f4aabc289845f114644dff90209fea14924aa0
mpivko <mpivko@yandex.ru>
2018-02-20 20:18:27 +0500

    Add gunicorn and docker

--- tasks/bank/pack-public-src.sh
+++ tasks/bank/pack-public-src.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+cd flask
+rm -f ../public-src.zip
+zip ../public-src.zip app.py features.py model.pickle requirements.txt
 
-zip public-src.zip app.py features.py model.pickle requirements.txt
