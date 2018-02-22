9427b38884552063cabca312a6f8776dfb82b193
mpivko <mpivko@yandex.ru>
2018-02-22 20:13:59 +0500

    Fix generator

--- tasks/cats-vs-dogs/generate.sh
+++ tasks/cats-vs-dogs/generate.sh
@@ -4,6 +4,6 @@ set -xe
 
 tar -xf cats.tar
 tar -xf dogs.tar
-./generate_flags.py 400 > flags.txt
-./build_images.py
+python3 generate_flags.py 400 > flags.txt
+python3 build_images.py
 rm -rf cats dogs
