4a06ca9a1e3a1c99d2ddd11f58f807a52bdb380f
1MiKHalyCH1 <bykoun@gmail.com>
2018-02-23 21:38:11 +0500

    mc flags changed

--- tasks/minecrypt/zip_generator.py
+++ tasks/minecrypt/zip_generator.py
@@ -11,7 +11,7 @@ ALPHA = digits+ascii_letters
 if __name__ == "__main__":
     seed(54345)
     ids = [''.join(choice(ALPHA) for _ in range(20)) for x in range(400)]
-    flags = ['QCTF{{{}}}'.format(''.join(choice(ALPHA) for _ in range(14))) for x in range(400)]
+    flags = ['QCTF{{{}}}'.format(''.join(choice(ALPHA) for _ in range(14))).upper() for x in range(400)]
     with open('ids_and_flags.py', 'w') as f:
         f.write('ids = {}\n'.format(repr(ids)))
         f.write('flags = {}'.format(repr(flags)))
