68a17e32d4f37eec210927ad8d63126aa0add79a
mpivko <mpivko@yandex.ru>
2018-02-23 17:49:03 +0500

    Init db after permission fixes

--- tasks/symlink-archive/flask/run-with-gunicorn.sh
+++ tasks/symlink-archive/flask/run-with-gunicorn.sh
@@ -6,4 +6,7 @@ chown -R www-data:www-data /var/user_data
 chown -R www-data:www-data /var/tmp_user_data
 chown -R www-data:www-data /var/db
 
+./generate_users.sh
+./initdb.sh
+
 gunicorn --config gunicorn.conf.py app:app
