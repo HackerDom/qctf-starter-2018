72893bd56120fea4f1f928ea76109a7dbfbd7d26
AZ404 <ztbzak1@rambler.ru>
2018-02-23 12:22:01 +0500

    increased number of teams

--- tasks/getflagchar/ids_generator.py
+++ tasks/getflagchar/ids_generator.py
@@ -1,13 +1,15 @@
 from hashlib import md5
 
+TEAMS_COUNT = 450
+
 def gen_task_id(team_id):
 	return md5(b"UYF2v976_R$^!F^@FGW*^$DQFWCjyrdhgj%d" % team_id).hexdigest()
 
 def gen_flag(team_id):
 	return "QCTF{%s}" % md5(b"bjhgyT^FR&Kv23F$Y#$UIfg2MsfgVE#SDY?DJY%d" % team_id).hexdigest()
 
-task_ids = [gen_task_id(team_id) for team_id in range(400)]
-flags = [gen_flag(team_id) for team_id in range(400)]
+task_ids = [gen_task_id(team_id) for team_id in range(TEAMS_COUNT)]
+flags = [gen_flag(team_id) for team_id in range(TEAMS_COUNT)]
 
 def main():
 	print(repr(task_ids))
--- tasks/getflagchar/main.c
+++ tasks/getflagchar/main.c
@@ -33,7 +33,7 @@ char getche(void)
 int main() {
     puts("Welcome to flag scanner! Enter your flag:");
 
-    int result = getche() == 'Q' && getche() == 'C' && getche() == 'T' && getche() == 'F' && getche() == '{' && getche() == '9' && getche() == '1' && getche() == 'd' && getche() == '8' && getche() == '9' && getche() == '5' && getche() == '6' && getche() == '3' && getche() == '7' && getche() == 'b' && getche() == 'f' && getche() == '2' && getche() == '1' && getche() == '8' && getche() == '4' && getche() == '9' && getche() == 'd' && getche() == '8' && getche() == 'a' && getche() == 'c' && getche() == '9' && getche() == '7' && getche() == '0' && getche() == '7' && getche() == '2' && getche() == 'd' && getche() == '9' && getche() == 'a' && getche() == 'a' && getche() == '6' && getche() == '6' && getche() == 'c' && getche() == '}';
+    int result = getche() == 'Q' && getche() == 'C' && getche() == 'T' && getche() == 'F' && getche() == '{' && getche() == '6' && getche() == 'd' && getche() == '3' && getche() == '6' && getche() == 'f' && getche() == '2' && getche() == '5' && getche() == '7' && getche() == 'd' && getche() == '2' && getche() == '4' && getche() == 'e' && getche() == 'e' && getche() == '0' && getche() == '6' && getche() == 'c' && getche() == 'c' && getche() == '4' && getche() == '0' && getche() == '2' && getche() == 'c' && getche() == '3' && getche() == 'b' && getche() == '1' && getche() == '2' && getche() == '5' && getche() == 'd' && getche() == '5' && getche() == 'e' && getche() == 'a' && getche() == 'a' && getche() == '7' && getche() == '}';
 
     putchar('\n');
 
