453255df32fb9eabca4220cf47b04a6b3c0eec6b
Arthur Liss <artliss96@gmail.com>
2018-02-25 00:36:50 +0500

    Unfinished tasks-deploy/bulls-and-cows

---
+++ tasks-deploy/bulls-and-cows/generate.py
@@ -0,0 +1,9 @@
+TITLE = 'Sample text: quirky-casino'
+STATEMENT = '''
+Sample text: quirky-casino
+[Site](http://quirky-casino.contest.qctf.ru/)
+'''
+
+
+def generate(context):
+    return TaskStatement(TITLE, STATEMENT)
