0dcaeb61613d4ea379ec8943b902f4d7c4b9e694
AZ404 <ztbzak1@rambler.ru>
2018-03-11 21:53:36 +0500

    added getflagchar resources

--- tasks/getflagchar/main.c
+++ tasks/getflagchar/main.c
@@ -33,7 +33,7 @@ char getche(void)
 int main() {
     puts("Welcome to flag scanner! Enter your flag:");
 
-    int result = getche() == 'Q' && getche() == 'C' && getche() == 'T' && getche() == 'F' && getche() == '{' && getche() == '6' && getche() == 'd' && getche() == '3' && getche() == '6' && getche() == 'f' && getche() == '2' && getche() == '5' && getche() == '7' && getche() == 'd' && getche() == '2' && getche() == '4' && getche() == 'e' && getche() == 'e' && getche() == '0' && getche() == '6' && getche() == 'c' && getche() == 'c' && getche() == '4' && getche() == '0' && getche() == '2' && getche() == 'c' && getche() == '3' && getche() == 'b' && getche() == '1' && getche() == '2' && getche() == '5' && getche() == 'd' && getche() == '5' && getche() == 'e' && getche() == 'a' && getche() == 'a' && getche() == '7' && getche() == '}';
+    int result = getche() == 'Q' && getche() == 'C' && getche() == 'T' && getche() == 'F' && getche() == '{' && getche() == 'e' && getche() == '5' && getche() == '1' && getche() == 'f' && getche() == '2' && getche() == 'd' && getche() == 'a' && getche() == 'd' && getche() == '8' && getche() == '7' && getche() == '8' && getche() == '7' && getche() == '5' && getche() == 'd' && getche() == '7' && getche() == '6' && getche() == 'f' && getche() == 'b' && getche() == '0' && getche() == '8' && getche() == '1' && getche() == 'f' && getche() == '2' && getche() == 'b' && getche() == '4' && getche() == '6' && getche() == '7' && getche() == '5' && getche() == '3' && getche() == '5' && getche() == 'e' && getche() == 'f' && getche() == '}';
 
     putchar('\n');
 
